98763c2e707409c100f347ca7d37b53d11745cee
soukaina <sokabn1@gmail.com>
2024-02-27 12:02:15 +0100

    intializing

---
+++ taxlotissement/src/main/java/com/example/taxlotissement/TaxlotissementApplication.java
@@ -0,0 +1,13 @@
+package com.example.taxlotissement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxlotissementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxlotissementApplication.class, args);
+	}
+
+}
---
+++ taxlotissement/src/test/java/com/example/taxlotissement/TaxlotissementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taxlotissement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxlotissementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
